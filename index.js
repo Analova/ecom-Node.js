8bab5a0fb82fccc193d0862c59ac6a574d3f99d5
Ana Nikolova <ani.nik19@gmail.com>
2020-02-03 10:20:50 +0100

    creating a web server and displaying simple html

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hi there");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+  <div>
+  <form>
+  <input palceholder="email">
+   <input palceholder="password">
+    <input palceholder="password confirmation">
+    <button>sign up</button>
+   </form>
+  </div>`);
+});
+
+app.listen(3000, () => {
+  console.log("listening");
+});
