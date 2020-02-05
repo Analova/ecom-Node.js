6da39fb4bc1d71a357702f0de34a4e32374723f0
Ana Nikolova <ani.nik19@gmail.com>
2020-02-05 13:00:55 +0100

    sigin and signout

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
+app.get("/signup", (req, res) => {
   res.send(`
   <div>
   Your id is:${req.session.userId}
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
   </div>`);
 });
 
-app.post("/", async (req, res) => {
+app.post("/signup", async (req, res) => {
   const { email, password, passwordConfirmation } = req.body;
   const existingUser = await usersRepo.getOneBy({ email });
   if (existingUser) {
@@ -41,6 +41,40 @@ app.post("/", async (req, res) => {
   // Store the id of that user inside the users cookie
   req.session.userId = user.id;
   res.send("Account created");
+});
+
+app.get("/signout", (req, res) => {
+  req.session = null;
+  res.send("You are logged out");
+});
+
+app.get("/signin", (req, res) => {
+  res.send(`
+  <div>
+  <form method="POST">
+  <input name="email" palceholder="email">
+   <input name="password" palceholder="password">
+    <button>Sign in</button>
+   </form>
+  </div>`);
+});
+
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send("Email not found!");
+  }
+
+  if (user.password !== password) {
+    return res.send("Invalid password");
+  }
+
+  req.session.userId = user.id;
+
+  res.send("You are signed in!");
 });
 
 app.listen(3000, () => {
