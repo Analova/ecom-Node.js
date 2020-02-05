44e6b58aa9a7639db742dd2645a46dbcaa2b48de
Ana Nikolova <ani.nik19@gmail.com>
2020-02-05 09:09:54 +0100

    creating user records and fething a session

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 const usersRepo = require("./repositories/users");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ["kjhggstv54262"]
+  })
+);
 
 app.get("/", (req, res) => {
   res.send(`
   <div>
+  Your id is:${req.session.userId}
   <form method="POST">
   <input name="email" palceholder="email">
    <input name="password" palceholder="password">
@@ -26,6 +33,13 @@ app.post("/", async (req, res) => {
   if (password !== passwordConfirmation) {
     return res.send("Passwords must match");
   }
+  // Create a user in our user repo to represent this person
+  const user = await usersRepo.create({
+    email,
+    password
+  });
+  // Store the id of that user inside the users cookie
+  req.session.userId = user.id;
   res.send("Account created");
 });
 
