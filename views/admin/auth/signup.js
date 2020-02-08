74a9f3b8b9c2e21c98f1b507325c4b155ffa089e
Ana Nikolova <ani.nik19@gmail.com>
2020-02-08 06:19:17 +0100

    display error messages

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,14 +1,25 @@
 const layout = require("../layout");
 
-module.exports = ({ req }) => {
+const getError = (errors, prop) => {
+  try {
+    return errors.mapped()[prop].msg;
+  } catch (errors) {
+    return "";
+  }
+};
+
+module.exports = ({ req, errors }) => {
   return layout({
     content: `
     <div>
   Your id is:${req.session.userId}
   <form method="POST">
   <input name="email" palceholder="email">
+  ${getError(errors, "email")}
    <input name="password" palceholder="password">
+   ${getError(errors, "password")}
     <input name="passwordConfirmation" palceholder="passwordConfirmation">
+    ${getError(errors, "passwordConfirmation")}
     <button>sign up</button>
    </form>
   </div>
